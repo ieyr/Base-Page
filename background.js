2f043d8c49e04c1b5ff8a32ac5eb403dbc233e01
Rohan Iyer <rohan.iyer1999@gmail.com>
2014-11-22 22:09:37 -0800

    redirect()

--- background.js
+++ background.js
@@ -5,10 +5,10 @@
 chrome.commands.onCommand.addListener(function(command) {
   console.log('onCommand event received for message: ', command);
   alert("gotpewevent");
-   chrome.extension.sendRequest({redirect: "http://google.com"});
+  redirect();
    alert("redirected to y");
 });
 
 function redirect(){
-	window.location.href = 'http://google.com';
+	window.location.href = 'www.google.com';
 }
