3fa422605cae655c7575d27578c284abf6f6ada8
Rohan Iyer <rohan.iyer1999@gmail.com>
2014-11-22 23:37:10 -0800

    Working Version 1.1

--- background.js
+++ background.js
@@ -5,28 +5,23 @@
 var myURL = "";
 var newurl = "";
 
+
+
 chrome.commands.onCommand.addListener(function(command) {
-	//alert(u);
-	alert(myURL);
   	console.log('onCommand event received for message: ', command);
-  	alert("gotpewevent");
-  	chrome.tabs.update({url: "http://google.com"});
-   	alert("redirected to " + myURL);
+  	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+	    myURL = tabs[0].url;
+	    
+	    newurl = myURL.substring(0,myURL.indexOf(".com")+4);
+	    alert(newurl);
+	    chrome.tabs.update({url: newurl});
+	});
+  	
 });
-
-//myurl="about:blank"; //A default url just in case below code doesn't work
-chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){ //onUpdated should fire when the selected tab is changed or a link is clicked 
-    chrome.tabs.getSelected(null,function(tab){
-        myURL=tab.url;
-    });
-});
-
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 
 
 function getNewUrl(oldurl){
-
+	newurl = myURL.substring(0,myURL.indexOf(".com")+4);
+	//alert(newurl)
 }
