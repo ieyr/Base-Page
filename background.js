293f438b4f31bd081f92c74abf9d2b64723ef129
Rohan Iyer <rohan.iyer1999@gmail.com>
2014-11-23 22:17:36 -0800

    progress?

--- background.js
+++ background.js
@@ -2,16 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
 var myURL = "";
 var newurl = "";
-
 
 
 chrome.commands.onCommand.addListener(function(command) {
   	console.log('onCommand event received for message: ', command);
   	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
 	    myURL = tabs[0].url;
-	    
+	    //alert(Mod.getPublicSuffix(myURL));
 	    newurl = myURL.substring(0,myURL.indexOf(".com")+4);
 	    //alert(myURL.indexOf(".com")+4);
 	    //if(!myURL.indexOf(".com")+4 == -1){ //attempt to stop it from not working outside of .com domains
